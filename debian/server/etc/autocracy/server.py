4179b6a2e59e74ef85040ee7a407696050470ec0
Wessel Dankers <wsl@fruit.je>
2024-02-19 20:04:18 +0100

    sort out file loading

--- debian/server/etc/autocracy/server.py
+++ debian/server/etc/autocracy/server.py
@@ -2,5 +2,5 @@ import os
 
 user = 'autocracy'
 base_dir = '/etc/autocracy/server'
-repository_root = '/etc/autocracy/repository'
+repository_root = '/etc/autocracy/server/repository'
 control_socket_path = f"{os.environ['RUNTIME_DIRECTORY']}/control"
