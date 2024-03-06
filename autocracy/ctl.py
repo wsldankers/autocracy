bfe040ce368358c40d5b970ecfbc261a89b357d6
Wessel Dankers <wsl@fruit.je>
2024-03-06 22:13:15 +0100

    fix ctl command output

--- autocracy/ctl.py
+++ autocracy/ctl.py
@@ -1,11 +1,15 @@
 import asyncio
 import aiohttp.web
 from os import getenv
+from itertools import chain
 
 from .rpc import RPC
+from .utils import warn
 
 
 async def main(procname, *args, **env):
+    exit_code = None
+
     async with aiohttp.ClientSession(
         raise_for_status=True,
         connector=aiohttp.UnixConnector(
@@ -23,11 +27,8 @@ async def main(procname, *args, **env):
                 async for _ in rpc:
                     raise RuntimeError("binary blob received")
 
-            run_task = asyncio.create_task(run_command())
-            rpc_task = asyncio.create_task(rpc_loop())
-
             done, pending = await asyncio.wait(
-                [run_task, rpc_task],
+                [asyncio.create_task(a()) for a in [run_command, rpc_loop]],
                 return_when=asyncio.FIRST_COMPLETED,
             )
 
@@ -35,3 +36,11 @@ async def main(procname, *args, **env):
             task.cancel()
 
         await asyncio.wait(pending)
+
+        for task in chain(done, pending):
+            exception = task.exception()
+            if exception is not None:
+                warn(exception)
+                exit_code = 1
+
+    return exit_code
--- bin/server.py
+++ bin/server.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python3
 
-from sys import argv
+from sys import argv, exit
 from os import environ
 from asyncio import run
 
 from autocracy.server import main
 
 if __name__ == '__main__':
-    run(main(*argv, **environ))
+    exit(run(main(*argv, **environ)))
